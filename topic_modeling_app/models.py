4438a8cefa8797f9a146a28e196ca566ccfd00a2
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2024-01-04 21:44:37 +0200

    Added params to LSA model

--- topic_modeling_app/models.py
+++ topic_modeling_app/models.py
@@ -8,3 +8,36 @@ class UserSelection(models.Model):
 class PertinentWords(models.Model):
     word = models.CharField(max_length=50)
     frequency = models.IntegerField()
+
+class LsaModel(models.Model):
+    num_topics = models.IntegerField()
+    chunksize = models.IntegerField()
+    decay = models.FloatField()
+    #distributed = models.BooleanField()
+    # onepass = models.BooleanField()
+    # power_iters = models.IntegerField()
+    # extra_samples = models.IntegerField()
+    # dtype = models.CharField(max_length=10)  # Assuming dtype is a string
+    # random_seed = models.IntegerField()
+
+    def __str__(self):
+        return f"LSA Model - {self.num_topics} topics, {self.chunksize} chunksize \
+            , {self.decay} decay"
+
+class LdaModel(models.Model):
+    num_topics = models.IntegerField()
+    chunksize = models.IntegerField()
+    # Other fields as needed
+
+    def __str__(self):
+        return f"LDA Model - {self.num_topics} topics, {self.passes} passes"
+
+
+    
+class PlsaModel(models.Model):
+    num_topics = models.IntegerField()
+    passes = models.IntegerField()
+    # Other fields as needed
+
+    def __str__(self):
+        return f"PLSA Model - {self.num_topics} topics, {self.passes} passes"
