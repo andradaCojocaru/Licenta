ece1ded20b70130e17a457e635a366981e48aa75
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2024-01-19 10:34:33 +0200

    Added header and footer

---
+++ topic_modeling_app/tasks.py
@@ -0,0 +1,6 @@
+# tasks.py
+from gensim import corpora, models
+
+def train_model_in_child_process(corpus_bow, dictionary, params, model_id, models_path):
+    model = models.LdaModel(corpus_bow, id2word=dictionary, **params)
+    model.save(os.path.join(models_path, model_id))
--- topic_modeling_project/urls.py
+++ topic_modeling_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from topic_modeling_app.views import select_options, bar_graph, topic_circles, lda_visualization,\
-      home, choose_model, model_detail, selected_parameters, add_corpus, process_corpus, train_lsi_button
+      home, choose_model, model_detail, selected_parameters, add_corpus, process_corpus, train_button
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('selected_parameters/<str:selected_model>/', selected_parameters, name='selected_parameters'),
     path('add_corpus/', add_corpus, name='add_corpus'),
     path('process_corpus/', process_corpus, name='process_corpus'),
-    path('train_lsi_button/', train_lsi_button, name='train_lsi_button'),
+    path('train_button/', train_button, name='train_button'),
     path('bar_graph/', bar_graph, name='bar_graph'),
     path('topic_circles/', topic_circles, name='topic_circles'),
     path('lda-visualization/', lda_visualization, name='lda_visualization'),
