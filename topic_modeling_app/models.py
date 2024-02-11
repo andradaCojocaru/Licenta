ace8b9c9a6b45f11becd89a3644b1d707abeea4b
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2024-02-11 22:49:10 +0200

    Add tooltips

--- topic_modeling_app/models.py
+++ topic_modeling_app/models.py
@@ -13,12 +13,12 @@ class LsaModel(models.Model):
     num_topics = models.IntegerField()
     chunksize = models.IntegerField()
     decay = models.FloatField()
-    #distributed = models.BooleanField()
-    # onepass = models.BooleanField()
-    # power_iters = models.IntegerField()
-    # extra_samples = models.IntegerField()
-    # dtype = models.CharField(max_length=10)  # Assuming dtype is a string
-    # random_seed = models.IntegerField()
+    distributed = models.BooleanField()
+    onepass = models.BooleanField()
+    power_iters = models.IntegerField()
+    extra_samples = models.IntegerField()
+    dtype = models.CharField(max_length=10)  # Assuming dtype is a string
+    random_seed = models.IntegerField()
 
     def __str__(self):
         return f"LSA Model - {self.num_topics} topics, {self.chunksize} chunksize \
@@ -29,19 +29,19 @@ class LdaModel(models.Model):
     chunksize = models.IntegerField()
     decay = models.FloatField()
     gamma_threshold = models.FloatField(null=True, blank=True)
-    #distributed = models.BooleanField()
-    # dtype = models.CharField(max_length=10)  # Assuming dtype is a string
-    # eval_every = models.IntegerField(null=True, blank=True)
-    # iterations = models.IntegerField(null=True, blank=True)
-    # gamma_threshold = models.FloatField(null=True, blank=True)
-    # minimum_probability = models.FloatField(null=True, blank=True)
-    # random_state = models.IntegerField(null=True, blank=True)
-    # minimum_phi_value = models.FloatField(null=True, blank=True)
-    # per_word_topics = models.BooleanField(default=False)
-    # passes = models.IntegerField(null=True, blank=True)  # Added passes as an integer field
-    # update_every = models.IntegerField(null=True, blank=True)  # Added update_every as an integer field
-    # alpha = models.FloatField(null=True, blank=True)  # Added alpha as a float field
-    # eta = models.FloatField(null=True, blank=True)  # Added eta as a float field
+    distributed = models.BooleanField()
+    dtype = models.CharField(max_length=10)  # Assuming dtype is a string
+    eval_every = models.IntegerField(null=True, blank=True)
+    iterations = models.IntegerField(null=True, blank=True)
+    gamma_threshold = models.FloatField(null=True, blank=True)
+    minimum_probability = models.FloatField(null=True, blank=True)
+    random_state = models.IntegerField(null=True, blank=True)
+    minimum_phi_value = models.FloatField(null=True, blank=True)
+    per_word_topics = models.BooleanField(default=False)
+    passes = models.IntegerField(null=True, blank=True)  # Added passes as an integer field
+    update_every = models.IntegerField(null=True, blank=True)  # Added update_every as an integer field
+    alpha = models.FloatField(null=True, blank=True)  # Added alpha as a float field
+    eta = models.FloatField(null=True, blank=True)  # Added eta as a float field
 
     def __str__(self):
         return f"LDA Model - {self.num_topics} topics, {self.chunksize} chunksize \
