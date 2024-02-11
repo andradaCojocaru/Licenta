768174ca9307a5e5c5f99a135167c6ffd5ab1b15
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2024-02-11 22:49:10 +0200

    Add tooltips

--- topic_modeling_app/forms.py
+++ topic_modeling_app/forms.py
@@ -22,16 +22,19 @@ class LsaModelForm(forms.ModelForm):
         model = LsaModel
         fields = '__all__'
         widgets = {
-            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'decay': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'distributed': forms.CheckboxInput(),
-            'onepass': forms.CheckboxInput(),
-            'power_iters': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'extra_samples': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'dtype': forms.TextInput(attrs={'placeholder': 'Optional'}),
-            'random_seed': forms.NumberInput(attrs={'placeholder': 'Optional'}),
+            'corpus': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Stream of document vectors or a sparse matrix of shape (num_documents, num_terms)'}),
+            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of requested factors (latent dimensions)'}),
+            'id2word': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'ID to word mapping'}),
+            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of documents to be used in each training chunk'}),
+            'decay': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Weight of existing observations relatively to new ones'}),
+            'distributed': forms.CheckboxInput(attrs={'title': 'If True - distributed mode (parallel execution on several machines) will be used'}),
+            'onepass': forms.CheckboxInput(attrs={'title': 'Whether the one-pass algorithm should be used for training. Pass False to force a multi-pass stochastic algorithm'}),
+            'power_iters': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of power iteration steps to be used. Increasing the number of power iterations improves accuracy, but lowers performance'}),
+            'extra_samples': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Extra samples to be used besides the rank k. Can improve accuracy'}),
+            'dtype': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Enforces a type for elements of the decomposed matrix'}),
+            'random_seed': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Random seed used to initialize the pseudo-random number generator, a local instance of numpy.random.RandomState instance'}),
         }
+
 
     def __init__(self, *args, **kwargs):
         super(LsaModelForm, self).__init__(*args, **kwargs)
@@ -71,22 +74,24 @@ class HdpModelForm(forms.ModelForm):
         model = HdpModel
         fields = '__all__'
         widgets = {
-            'max_chunks': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'max_time': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'kappa': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'tau': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'K': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'T': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'alpha': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'gamma': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'eta': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'scale': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'var_converge': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'outputdir': forms.TextInput(attrs={'placeholder': 'Optional'}),
-            'random_state': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            # Add more widgets for other fields if needed
+            'corpus': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Corpus in BoW format'}),
+            'id2word': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Dictionary for the input corpus'}),
+            'max_chunks': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Upper bound on how many chunks to process. It wraps around corpus beginning in another corpus pass, if there are not enough chunks in the corpus'}),
+            'max_time': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Upper bound on time (in seconds) for which model will be trained'}),
+            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of documents in one chunk'}),
+            'kappa': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Learning parameter which acts as exponential decay factor to influence extent of learning from each batch'}),
+            'tau': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Learning parameter which down-weights early iterations of documents'}),
+            'K': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Second level truncation level'}),
+            'T': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Top level truncation level'}),
+            'alpha': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Second level concentration'}),
+            'gamma': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'First level concentration'}),
+            'eta': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'The topic Dirichlet'}),
+            'scale': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Weights information from the mini-chunk of corpus to calculate rhot'}),
+            'var_converge': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Lower bound on the right side of convergence. Used when updating variational parameters for a single document'}),
+            'outputdir': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Stores topic and options information in the specified directory'}),
+            'random_state': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Adds a little random jitter to randomize results around same alpha when trying to fetch a closest corresponding lda model from suggested_lda_model()'}),
         }
+
 
     def __init__(self, *args, **kwargs):
         super(HdpModelForm, self).__init__(*args, **kwargs)
@@ -99,20 +104,22 @@ class NmfModelForm(forms.ModelForm):
         model = NmfModel
         fields = '__all__'
         widgets = {
-            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'passes': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'kappa': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'minimum_probability': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'w_max_iter': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'w_stop_condition': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'h_max_iter': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'h_stop_condition': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'eval_every': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'normalize': forms.CheckboxInput(),
-            'random_state': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            # Add more widgets for other fields if needed
+            'corpus': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Training corpus. Can be either iterable of documents, which are lists of (word_id, word_count), or a sparse csc matrix of BOWs for each document. If not specified, the model is left uninitialized'}),
+            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of topics to extract'}),
+            'id2word': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'Mapping from word IDs to words. Used to determine the vocabulary size, as well as for debugging and topic printing'}),
+            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of documents to be used in each training chunk'}),
+            'passes': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of full passes over the training corpus'}),
+            'kappa': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Gradient descent step size. Larger value makes the model train faster, but could lead to non-convergence if set too large'}),
+            'minimum_probability': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'If normalize is True, topics with smaller probabilities are filtered out. If normalize is False, topics with smaller factors are filtered out. If set to None, a value of 1e-8 is used to prevent 0s'}),
+            'w_max_iter': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Maximum number of iterations to train W per each batch'}),
+            'w_stop_condition': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'If error difference gets less than that, training of W stops for the current batch'}),
+            'h_max_iter': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Maximum number of iterations to train h per each batch'}),
+            'h_stop_condition': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'If error difference gets less than that, training of h stops for the current batch'}),
+            'eval_every': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of batches after which l2 norm of (v - Wh) is computed. Decreases performance if set too low'}),
+            'normalize': forms.CheckboxInput(attrs={'title': 'Whether to normalize the result. Allows for estimation of perplexity, coherence, etc.'}),
+            'random_state': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Seed for random generator. Needed for reproducibility'}),
         }
+
 
     def __init__(self, *args, **kwargs):
         super(NmfModelForm, self).__init__(*args, **kwargs)
