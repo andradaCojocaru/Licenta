695ad0b3d481a31e94b7bbacbbf1fa078a7a4b8b
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2024-02-11 22:19:35 +0200

    Changes design

--- topic_modeling_app/forms.py
+++ topic_modeling_app/forms.py
@@ -43,18 +43,27 @@ class LdaModelForm(forms.ModelForm):
         model = LdaModel
         fields = '__all__'
         widgets = {
-            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'decay': forms.NumberInput(attrs={'placeholder': 'Optional'}),
-            'gamma_threshold': forms.NumberInput(attrs={'step': 'any'}),
-            'dtype': forms.Select(choices=[('float32', 'float32'), ('float64', 'float64')]),  # Adjust choices based on your needs
-            # Add more widgets for other fields if needed
+            'num_topics': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'The number of requested latent topics to be extracted from the training corpus'}),
+            'chunksize': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of documents to be used in each training chunk'}),
+            'passes': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of passes through the corpus during training'}),
+            'update_every': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Number of documents to be iterated through for each update. Set to 0 for batch learning, > 1 for online iterative learning'}),
+            'alpha': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'A-priori belief on document-topic distribution. Scalar for a symmetric prior over document-topic distribution, 1D array of length equal to num_topics to denote an asymmetric user-defined prior for each topic, or default prior selecting strategies'}),
+            'eta': forms.TextInput(attrs={'placeholder': 'Optional', 'title': 'A-priori belief on topic-word distribution. Scalar for a symmetric prior over topic-word distribution, 1D array of length equal to num_words to denote an asymmetric user-defined prior for each word, matrix of shape (num_topics, num_words) to assign a probability for each word-topic combination, or default prior selecting strategies'}),
+            'decay': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'A number between (0.5, 1] to weight what percentage of the previous lambda value is forgotten when each new document is examined'}),
+            'offset': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Hyper-parameter that controls how much the first steps are slowed down in the first few iterations'}),
+            'eval_every': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Log perplexity is estimated every that many updates. Setting this to one slows down training by ~2x'}),
+            'iterations': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Maximum number of iterations through the corpus when inferring the topic distribution of a corpus'}),
+            'gamma_threshold': forms.NumberInput(attrs={'step': 'any', 'title': 'Minimum change in the value of the gamma parameters to continue iterating'}),
+            'minimum_probability': forms.NumberInput(attrs={'step': 'any', 'title': 'Topics with a probability lower than this threshold will be filtered out'}),
+            'random_state': forms.NumberInput(attrs={'placeholder': 'Optional', 'title': 'Seed to generate a randomState object for reproducibility'}),
+            'dtype': forms.Select(choices=[('float16', 'float16'), ('float32', 'float32'), ('float64', 'float64')], attrs={'title': 'Data type to use during calculations inside the model'}),
         }
 
     def __init__(self, *args, **kwargs):
         super(LdaModelForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.required = False
+
         # Set other fields as not required if needed
 
 class HdpModelForm(forms.ModelForm):
